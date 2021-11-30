6f51d7c0dd9074bf93c4eda0f4f0212dd988b061
Gulnaz11 <81852862+Gulnaz11@users.noreply.github.com>
2021-11-30 15:02:55 +0300

    example

--- Node/script.js
+++ Node/script.js
@@ -7,3 +7,27 @@
 //console.log(os.type());
 
 //Работа с файлами
+
+//const fs=require("fs");
+//let users=[{'name':'Иван', 'id':10}];
+//
+//
+//fs.readFile('Node/test.json','utf-8',(err,data)=>{//data-это то, что мы считали из файла
+//    if(err){
+//         console.log(err);
+//    }else{
+//        let users=JSON.parse(data);
+//        let user='{"name":"Петр","id":15}';
+//        users.push(JSON.parse(user));
+//        fs.writeFile('Node/test.json',JSON.stringify(users),err=>{
+//            if(err){
+//                console.log(err);
+//            }
+//        });
+//    }
+//}
+//)
+
+//Внешние модули
+//const moment=require('moment');
+//console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
